35760d4aa0bb178666ed6fd1dd424e972ce19b01
BSDinis <baltasardinis@hotmail.com>
2018-11-08 13:17:35 +0000

    updated test scripts

--- bin/doTest.sh
+++ bin/doTest.sh
@@ -3,6 +3,7 @@
 echo "==          DO TEST         =="
 echo "==  run tests on ParSolver  =="
 echo "== max number of threads: "$1" =="
+echo "== inpufile: "$2" =="
 
 echo "== Running sequential =="
 ../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver $2
@@ -12,6 +13,8 @@ for i in {1..$1}
 do
   echo "== Running parallel w/ "$i" threads =="
   ../CircuitRouter-ParSolver/CircuitRouter-ParSolver -t $i $2 ;
+  grep routed ${2}.res.old
+  grep routed ${2}.res
   echo "== Done == "
 done
 
