e86b57c99c1cec889064daed18d4adfd88a385e2
BSDinis <baltasardinis@hotmail.com>
2018-11-08 13:05:40 +0000

    added a script to save typing

---
+++ bin/doTheOneTrueTest.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/zsh
+#!/usr/bin/bash
+
+# got tired of thinking which file to run the project on
+
+file=../inputs/random-x48-y48-z3-n64.txt
+
+echo "============================================================"
+echo " == SOLVING FILE " $file
+echo "============================================================"
+echo
+./doTest.sh $1 $file
+
+echo "============================================================"
+echo " == CHECKING " $file
+echo "============================================================"
+diff ${file}.res.old ${file}.res
+
+
