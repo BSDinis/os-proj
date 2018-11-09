f3f1076ff1c4a77efc3b1ae105da7441637f3bae
BSDinis <baltasardinis@hotmail.com>
2018-11-09 10:55:13 +0000

    doTest is up

--- bin/doTest.sh
+++ bin/doTest.sh
@@ -8,6 +8,7 @@ then
 fi
 
 speedup_file=$2.speedups.csv
+new_speedup_file=../results/$(basename ${speedup_file})
 
 extract_time () {
   echo $timed
@@ -16,7 +17,7 @@ extract_time () {
 
 add_new_entry () {
   speedup=$(echo "scale=6; ${sequential}/$3" | bc)
-  echo $2", "$3", "${speedup} >> ${speedup_file}
+  echo $2",\t\t"$3",\t"${speedup} >> ${speedup_file}
 }
 
 
@@ -24,13 +25,13 @@ echo "==          DO TEST         =="
 echo "==  run tests on ParSolver  =="
 echo "== max number of threads: "$1" =="
 echo "== inpufile: "$2" =="
-echo "== speedup_file: "${speedup_file}" =="
+echo "== speedup_file: "${new_speedup_file}" =="
 
 
 # create empty file
 rm -f ${speedup_file}
 touch ${speedup_file}
-echo "#nofthreads, time, speedup" >> ${speedup_file}
+echo "#nofthreads,\ttime,\t\tspeedup" >> ${speedup_file}
 
 echo "== Running sequential =="
 ../CircuitRouter-SeqSolver/CircuitRouter-SeqSolver $2
@@ -45,5 +46,9 @@ do
   ../CircuitRouter-ParSolver/CircuitRouter-ParSolver -t $i $2 ;
   timed=$(grep "Elapsed time" $2.res | cut -d " " -f 5)
   add_new_entry $2.res $i $timed
+  echo "== Time: "$timed
   echo "== Done == "
 done
+
+# This should be a move, but my hands were tied
+cp $speedup_file $new_speedup_file
