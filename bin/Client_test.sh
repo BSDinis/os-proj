44e0c1ea8d5c4dcd96da46bb54a30ba8956de267
BSDinis <baltasardinis@hotmail.com>
2018-11-30 19:55:52 +0000

    updated testing script

---
+++ bin/Client_test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/zsh
+
+if ! [[ -x ../CircuitRouter-AdvShell/CircuitRouter-AdvShell ]]
+then
+  echo "Found no executable"
+  echo "abort"
+  exit 1
+fi
+
+./gen_shell_inputs.py $1 $2 > .a.tmp
+
+echo "====================================="
+echo "=========== Regular Tests ==========="
+echo "====================================="
+../CircuitRouter-AdvShell/CircuitRouter-AdvShell &;
+../CircuitRouter-Client/CircuitRouter-Client ../CircuitRouter-AdvShell/CircuitRouter-AdvShell.pipe < .a.tmp
+
+: '
+echo "====================================="
+echo "=========== Valgrind Tests =========="
+echo "====================================="
+valgrind ../CircuitRouter-AdvShell/CircuitRouter-AdvShell < .a.tmp
+echo "" > .a.tmp
+
+echo "====================================="
+echo "============= Extra Tests ==========="
+echo "====================================="
+valgrind ../CircuitRouter-AdvShell/CircuitRouter-AdvShell < .a.tmp
+
+'
+rm .a.tmp
+
+
